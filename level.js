b31e538aac07ac16bc6fafb7bb953216dcde4996
nhondt2 <nhondt2@vng.com.vn>
2015-10-12 02:48:29 +0700

    zxvzx

--- level.js
+++ level.js
@@ -30,13 +30,13 @@ var test1 = [
     {
         "top": -1, "left": -1, "bottom": -1, "right": 1, "value": "H","idx":0, "initX":100,"initY":100
     }, {
-        "top": -1, "left": -1, "bottom": -1, "right": 2, "value": "E","idx":1
+        "top": -1, "left": -1, "bottom": -1, "right": -1, "value": "E","idx":1
     },{
-        "top": -1, "left": -1, "bottom": -1, "right": -1, "value": "L","idx":2
+        "top": -1, "left": -1, "bottom": -1, "right": 3, "value": "L","idx":2, "initX":50,"initY":200
     }, {
-        "top": -1, "left": -1, "bottom": -1, "right": 4, "value": "L","idx":3, "initX":50,"initY":200
+        "top": -1, "left": -1, "bottom": -1, "right": -1, "value": "L","idx":3
     },{
-        "top": -1, "left": -1, "bottom": -1, "right": -1, "value": "O","idx":4
+        "top": -1, "left": -1, "bottom": -1, "right": -1, "value": "O","idx":4, "initX":200,"initY":200
     }, {
         "top": -1, "left": -1, "bottom": 6, "right": -1, "value": "O","idx":5, "initX":250,"initY":250
     },{
@@ -44,4 +44,7 @@ var test1 = [
     }, {
         "top": -1, "left": -1, "bottom": -1, "right": -1, "value": "G","idx":7
     },
+]
+var answer=[
+    [0,1,2,3,4],[2,5,6,7]
 ]
